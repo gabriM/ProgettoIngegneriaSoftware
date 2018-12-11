1b39a35044dacc4f723ae2b62b2b6aab8f47aaf2
MatteoGusmini <45758666+MatteoGusmini@users.noreply.github.com>
2018-12-11 11:10:36 +0100

    Prova guarda se nel main vedi il commento che ho aggiunto

--- Ingegneria_Software_1.0/src/Main.java
+++ Ingegneria_Software_1.0/src/Main.java
@@ -6,7 +6,7 @@ import MyLib.Utility;
 public class Main {
 
 	public static void main(String[] args) {
-
+		// prova di uso github
 		
 		// Costanti che contengono i messaggi da visualizzare in Output
 
